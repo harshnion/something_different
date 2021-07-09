9b61979f7d40466d433a0ad48f072826085d51bd
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-09 23:58:49 +0530

    15_commit

---
+++ different.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+n = int(raw_input())
+for i in range(1,n+1):
+  print(i,end='');
+# In this we have all print values/output in one line by using end it can be done
